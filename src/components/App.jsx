b1e93f87b079b12e37d93d3bd37a2d18bd32d431
chm <3036643270@qq.com>
2020-12-01 10:57:25 +0800

    battle

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { HashRouter as Router, Link, Route } from "react-router-dom";
+import { hot } from "react-hot-loader/root";
+import Popular from "../pages/popular.jsx";
+import Battle from "../pages/battle.jsx";
+import BattleResult from "../pages/battleResult.jsx";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="Nav">
+            <Link to="/">Popular</Link>
+            <Link to="/battle" style={{ marginLeft: "20px" }}>
+              Battle
+            </Link>
+          </div>
+          <hr />
+          <Route path="/" exact component={Popular}></Route>
+          <Route path="/battle" exact component={Battle}></Route>
+          <Route path="/battle/:name" exact component={BattleResult}></Route>
+        </div>
+      </Router>
+    );
+  }
+}
+export default hot(App);
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "@babel/polyfill";
+import App from "./components/App.jsx";
+import "./styles/index.less";
+
+// import 'normalize.css/normalize.css';
+
+ReactDOM.render(<App />, document.getElementById("container"));
